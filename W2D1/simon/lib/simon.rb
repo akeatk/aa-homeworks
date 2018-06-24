d028e031c1830f4886ec2abdb28f3eb3ec7d86bf
Alex <akeatk@gmail.com>
2018-06-24 14:19:09 -0700

    Functioning game and passes spec

--- W2D1/simon/lib/simon.rb
+++ W2D1/simon/lib/simon.rb
@@ -12,12 +12,14 @@ class Simon
   def play
     until @game_over
       take_turn
+      sleep 1
     end
     game_over_message
     reset_game
   end
 
   def take_turn
+    starting_warning
     show_sequence
     require_sequence
     unless @game_over
@@ -26,15 +28,39 @@ class Simon
     end
   end
 
+  def starting_warning
+    system "clear"
+    print "starting"
+    4.times do |i|
+      sleep(1.0/4.0)
+      print "."
+    end
+  end
+
   def show_sequence
     add_random_color
+    @seq.each do |el|
+      system "clear"
+      sleep(1.0/4.0)
+      print "#{el}"
+      sleep 1
+    end
+    system "clear"
+    $stdout.flush
   end
 
   def require_sequence
     input = nil
     while true
-      print "Please enter the color of the next "
-
+      print "Please enter the sequence of colors: "
+      input = gets.downcase.split
+      if input.all? {|el| COLORS.include?(el)}
+        break
+      end
+      puts "Improper input. Please check spelling."
+    end
+    for i in 0...@seq.length
+      @game_over = true if @seq[i] != input[i]
     end
   end
 
@@ -56,3 +82,7 @@ class Simon
     @game_over = false
   end
 end
+
+#used for testing the full game
+#game = Simon.new
+#game.play
